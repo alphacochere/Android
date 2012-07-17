6a66cfca117fb308e15f9cb627dab53d8011f7d2
iluron <nic0-42@homtail.fr>
2012-07-17 20:22:09 +0200

    Premiere modif Nico

--- PremiereAppli/src/com/example/premiereappli/MainActivity.java
+++ PremiereAppli/src/com/example/premiereappli/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.premiereappli;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView text = null;
+	String hello = null;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+		hello = getResources().getString(R.string.hello_world);
+		hello = hello.toUpperCase();
+
+		text = new TextView(this);
+		text.setText(hello);
+
+		setContentView(text);
+        
     }
 
     @Override
@@ -20,5 +33,11 @@ public class MainActivity extends Activity {
         return true;
     }
 
+
+
+
+
+
+    
     
 }
