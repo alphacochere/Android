530c75f08a194b97dd5049c614419d63e277f6f3
iluron <nic0-42@homtail.fr>
2012-07-19 02:09:10 +0200

    recup textEdit + affichage popup et textView
    J'ai créé un formatter de code sous eclipse je te l'ai dropé avec un
    read me pour l'installer.
    Comme ca notre code sera toujour nikel en indentation : )

--- PremiereAppli/src/com/example/premiereappli/MainActivity.java
+++ PremiereAppli/src/com/example/premiereappli/MainActivity.java
@@ -3,40 +3,56 @@ package com.example.premiereappli;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-	TextView text = null;
-	String hello = null;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-		hello = getResources().getString(R.string.hello_world);
-		hello = hello.toUpperCase();
+	private Button buttonRecupNombre;
+	private EditText textRecupNombre;
+	private String chaine;
+	private Integer nombre;
 
-		text = new TextView(this);
-		text.setText(hello);
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
 
-		setContentView(text);
-        
-    }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		buttonRecupNombre = (Button) findViewById(R.id.button1);
+		textRecupNombre = (EditText) findViewById(R.id.editText1);
 
+		// listener sur le bouton
+		buttonRecupNombre.setOnClickListener(new OnClickListener()
+		{
 
+			@Override
+			public void onClick(View v)
+			{
+				chaine = textRecupNombre.getText().toString();
+				nombre = Integer.parseInt(chaine);
 
+				// on affiche le nombre dans une petite pop-up qui s'affiche quelques seconde en bas d'Žcran
+				Toast.makeText(MainActivity.this, "Le nombre = " + chaine + " !", Toast.LENGTH_LONG).show();
 
+				// affichage du nombre dans notre textView
+				((TextView) findViewById(R.id.textView1)).setText("Le nombre = " + chaine + " !");
+			}
 
+		});
+	}
 
-    
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
