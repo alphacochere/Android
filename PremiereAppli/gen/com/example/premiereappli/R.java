1f6f7d76d2b76b523e5b09f7ce4a7a062fefe2cf
iluron <nic0-42@homtail.fr>
2012-09-06 23:26:58 +0200

    landscape

--- PremiereAppli/gen/com/example/premiereappli/R.java
+++ PremiereAppli/gen/com/example/premiereappli/R.java
@@ -28,6 +28,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_main_landscape=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
